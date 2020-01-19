34cb4f394cf80299ed498a3719c40e49428c42a6
Pritam Singh <Pritams@campusmgmt.com>
2020-01-19 13:56:58 +0530

    End of Section 6 : Alertifyjs and app theame

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-import { type } from 'os';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
